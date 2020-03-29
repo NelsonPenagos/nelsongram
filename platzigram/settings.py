42d6842cd759106eb4aee27796d7eae68bb1855f
Nelson <nelson.penagosj@gmail.com>
2020-03-29 16:28:50 -0500

    add forms and templates

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -133,5 +133,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_URL = '/users/login/'
--- users/views.py
+++ users/views.py
@@ -1,23 +1,48 @@
 """Users views."""
 
 # Django
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Exception
-from django.db.utils import IntegrityError
+# Forms
+from users.forms import ProfileForm, SignupForm
 
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
 
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
-    return render(request, 'users/update_profile.html')
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+
+    else:
+        form = ProfileForm()
+
+    return render(
+        request=request,
+        template_name='users/update_profile.html',
+        context={
+            'profile': profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
 
 def login_view(request):
-    """Login view"""
+    """Login view."""
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -30,32 +55,23 @@ def login_view(request):
 
     return render(request, 'users/login.html')
 
+
 def signup(request):
     """Sign up view."""
     if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+    return render(
+        request=request,
+        template_name='users/signup.html',
+        context={'form': form}
+    )
 
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
 
 @login_required
 def logout_view(request):
